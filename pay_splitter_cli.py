8071420e0b75c9dec3241544037bf627a4253621
Robert Forget <robforget@gmail.com>
2021-07-21 20:34:38 -0400

    Added way to account for borrowed money

--- pay_splitter_cli.py
+++ pay_splitter_cli.py
@@ -1,6 +1,13 @@
 # Y is P% of X 
 # X/Y = P x 100
 #flags to add= does dad owe money? bool
+print('Does your dad owe you money? if so put how much. If not, type "no"')
+doesdadowe = input()
+if doesdadowe != "no":
+    dadpay = doesdadowe
+else:
+    print("Thats a nice change of pace!")
+   
 print("Input total of invoice(s) you're splitting: ")
 total = input()
 percent = .20
@@ -11,6 +18,12 @@ netpay = float(total) - result
 splitpay = netpay / 2
 splitpay = round(splitpay, 2)
 netpay = round(splitpay, 2) 
-print(f"Split pay is: {float(splitpay)}$ Each.")
+robpay = splitpay
+if doesdadowe != "no":
+  dadpay = splitpay - float(doesdadowe)
+  print (f"Dad's pay is:{float(dadpay)}$")
+
+print(f"Split pay is: {splitpay}$ Each.")
+
 #test2
 #test3
